4bd7939980d808d9a18ee2973eb7798b6933d2a9
Boggala Ramanjaneyulu <2000173132@hexaware.com>
2025-12-18 15:18:55 +0530

    First commit

---
+++ src/main/java/com/javaguide/springboot/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.javaguide.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Spring boot application!";
+    }
+}
